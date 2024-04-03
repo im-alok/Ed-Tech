314920403c7248fba23d556db4d5993fbdfe7bf1
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2024-04-03 23:57:28 +0530

    adding ty page

---
+++ src/Components/common/ThankYouPage.jsx
@@ -0,0 +1,22 @@
+function ThankYouPage(){
+    return(
+        <div class=" content ">
+            <div class="">
+                <div class="wrapper-2">
+                    <h1>Thank you !</h1>
+                    <p>Thanks for subscribing to ConnectChat.  </p>
+                    <p>we will get back to you very soon </p>
+                    <button class="go-home">
+                        go home
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    )
+}
+
+export default ThankYouPage
